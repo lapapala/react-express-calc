c2dd5793e601ac7df10136227967bca4aa3d1dca
Oleh Vaskiv <olegvaskiw@ukr.net>
2019-05-14 16:28:43 +0300

    first commit

---
+++ api/app.js
@@ -0,0 +1,104 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var cors = require("cors");
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var testAPIRouter = require("./routes/testAPI");
+
+var express = require("express");
+var app = express();
+var PORT = 8080;
+
+
+app.get("/", function(req, res){
+	res.send("hello! enter your value ")
+});
+
+//listener
+app.listen(PORT, function(){
+	console.log("App listening on PORT:" + PORT);
+});
+
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(cors());
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.get('/:operator/:num1/:num2', function(req,res){
+	console.log(req, res)
+	var operator = req.params.operator
+	var num1 = parseInt(req.params.num1);
+	var num2 = parseInt(req.params.num2);
+	var result;
+
+	switch(operator){
+		case "addition":
+		result = add(num1, num2);
+		break;
+
+		case "subtraction":
+		result = subtract(num1, num2);
+		break;
+
+		case "multiplication":
+		result = multiply(num1, num2);
+		break;
+
+		case "division":
+		result = divide(num1, num2);
+		break;
+
+		default:
+		result = "Invalid value"
+	}
+
+	function add(a, b){
+		return a + b
+	}
+
+	function subtract(a, b){
+		return a - b
+	}
+
+	function multiply(a, b){
+		return a * b
+	}
+
+	function divide(a, b){
+		return a / b
+	}
+
+	res.json(result);
+});
+//app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use("/testAPI", testAPIRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+      this.state = { 
+        apiResponse: "", 
+        answer: "",
+        inputValue: ""
+      };
+      
+    }
+    callAPI() {
+      fetch("http://localhost:9000/testAPI")
+      .then(res => res.text())
+            .then(res => this.setState({ apiResponse: res }));
+    }
+    componentWillMount() {
+        this.callAPI();
+    }
+
+    result = () => {
+      fetch("http://localhost:9000/division/2/2")
+        .then(res => res.text())
+        .then(res => this.setState({ answer: res }));
+    }
+
+    // input = () => {
+    //     this.refs.input.value
+    // }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+
+            <label>
+              Enter your example 
+              <input  className="input" type="text" name="name" value={this.state.inputValue} />
+            </label>
+
+            <button onClick={this.result} >Calculate</button>
+
+            <label>
+              Result 
+              <input className="input" type="text" name="name"  value={this.state.answer} onChange={() => {}} />
+            </label>
+       
+        </header>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
